1f04b2639b2c1aba2fc155381baa1b40e109c877
worldbeater2 <realdudization@gmail.com>
2024-07-05 02:26:53 +0000

    minor fix on taxbar

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <>
      <ScrollToTop />
       <Routes>
-        <Route path="/" element={<Signup />} />
+        <Route path="/" element={<Homepage />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/blog" element={<Blog />} />
