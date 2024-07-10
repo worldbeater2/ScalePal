206c8faaa77fde2bb5c2b985ff73f1c7693f2923
worldbeater2 <realdudization@gmail.com>
2024-07-10 21:38:50 +0000

    google auth fix

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Signup from './pages/Signup';
 import ScrollToTop from './units/ScrollToTop';
 import ProtectedRoute from './units/ProtectedRoute';
 
+
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [user, setUser] = useState(null);
